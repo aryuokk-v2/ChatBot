e30cc549839f6c82219c7d8e243fef71c739362a
Aryan <84660211+stark-Prince@users.noreply.github.com>
2021-09-27 16:44:07 +0530

    Update __init__.py

--- NobitaChatBot/__init__.py
+++ NobitaChatBot/__init__.py
@@ -2,8 +2,8 @@ import os
 
 from pyrogram import Client
 
-API_ID = os.environ.get("API_ID", 2329393)
-API_HASH = os.environ.get("API_HASH", "97edb95abc9a61fecea49ef8b31de8f3")
-TOKEN = os.environ.get("TOKEN", "2037975975:AAEm__YIo_6W1rSXXag23DsstoCX2mbB07w")
+API_ID = os.environ.get("API_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+TOKEN = os.environ.get("TOKEN", None)
 
 Nobita = Client(":memory:", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
