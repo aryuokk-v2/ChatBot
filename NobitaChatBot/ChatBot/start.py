57ec086cde9945cac6431b4fe1555ed0a0280a3d
Aryan <84660211+stark-Prince@users.noreply.github.com>
2021-09-27 16:42:02 +0530

    Add files via upload

---
+++ NobitaChatBot/ChatBot/start.py
@@ -0,0 +1,26 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from NobitaChatBot import Nobita
+
+NOBITA_START = """
+I am Nobita, An Intelligent ChatBot.[⠀](https://telegra.ph/file/7ab6d51c24bef8396b752.png)
+"""
+
+
+@Nobita.on_message(
+    filters.command(["start"], prefixes=["/", "!"]) & ~filters.edited)
+async def info(client, message):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                "Developer",
+                url="https://t.me/its_Prince"),
+            InlineKeyboardButton("Join",
+                                 url="https://t.me/HaramiSquad"),
+        ],
+    ]
+    await Nobita.send_message(
+        chat_id=message.chat.id,
+        text=NOBITA_START,
+        reply_markup=InlineKeyboardMarkup(buttons),
+    )
---
+++ NobitaChatBot/__main__.py
@@ -0,0 +1,7 @@
+from pyrogram import filters
+
+from NobitaChatBot import Nobita, ChatBot
+from NobitaChatBot.ChatBot import bot, start
+
+if __name__ == "__main__":
+    Nobita.run()
